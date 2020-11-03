fac8337e6f1e77adb7af3a3816541c79daabbed8
isadorasouzza <isadora.souzabantonio@gmail.com>
2020-11-02 22:56:47 -0300

    Implementação da interface do projeto

--- Mineração de Tweet.py
+++ Mineração de Tweet.py
@@ -13,6 +13,6 @@ csvFile = open('TweetsCovid.csv', 'a', encoding="utf-8-sig")
 csvWriter = csv.writer(csvFile)
 query_search = "#covid OR #quaretena OR #corona" + " -filter:retweets"
 
-for tweet in tw.Cursor(api.search,q=query_search,lang="pt").items(100):
+for tweet in tw.Cursor(api.search,q=query_search,lang="pt", since="2020-10-01").items(100):
     print (tweet.text)
     csvWriter.writerow([tweet.text])
